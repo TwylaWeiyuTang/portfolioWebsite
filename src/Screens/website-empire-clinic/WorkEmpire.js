96fe84c17058451f62d5ba2a701de52791521c6e
Weiyu Tang <twyla9876@outlook.com>
2022-09-07 23:06:20 +0100

    fix button color on work detail page

--- src/Screens/website-empire-clinic/WorkEmpire.js
+++ src/Screens/website-empire-clinic/WorkEmpire.js
@@ -54,9 +54,14 @@ const Button = styled.div`
       font-size: 1.5rem;
       border: 2px solid black;
       padding: 5px 30px 5px 30px;
+      color: black;
       border-radius: 30px;
       background-color: transparent;
       cursor: default;
+
+      a {
+        color: black;
+      }
 
       :nth-child(1) {
         border: none;
@@ -162,7 +167,7 @@ const WorkEmpire = () => {
     <Section style={{backgroundColor: "#eadede"}}>
       <Button data-scroll data-scroll-speed='-6'>
         <button><a href='https://www.empireclinic.it/' target='_blank' rel='noreferrer'><svg className="text" width="100%" height="80px" aria-hidden="true">
-          <text className="text" x="0" y="0" dy="1em" fill="currentColor">View the live site</text>
+          <text className="text" x="0" y="0" dy="1em" fill="black">View the live site</text>
           <path className="stroke" style={{transform: "translateY(1em) scale(1)"}} fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" stroke-miterlimit="10" d="M16.7 20.2c76.5 4.4 153.6-9.7 229.8-4.1 5.4.4 12.4 2.1 11.7 5.6-67.3 1.7-134.5 5.5-201.2 11.5l87.7-.9c35.2-.4 70.8-.7 104.9 4.6"></path>
         </svg>
         </a>
