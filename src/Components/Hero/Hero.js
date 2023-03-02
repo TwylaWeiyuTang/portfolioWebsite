0c173593e1a3e68acaa1df47ff95f184059ecdb0
Weiyu Tang <twyla9876@outlook.com>
2023-03-02 17:02:34 +0000

    changed the style of the footer, and added the animation for the footer part

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,178 +1,195 @@
-import React, {useRef, useEffect} from 'react'
-import './heroStyle.scss'
-import styled from 'styled-components'
-import gsap from 'gsap'
+import React, { useRef, useEffect } from "react";
+import "./heroStyle.scss";
+import styled from "styled-components";
+import gsap from "gsap";
 
 const Hero = () => {
-        let app = useRef(null)
-        let title = useRef(null)
+  let app = useRef(null);
+  let title = useRef(null);
 
-        let tl = new gsap.timeline({delay: .8})
+  let tl = new gsap.timeline({ delay: 0.8 });
 
-        useEffect(() => {
+  useEffect(() => {
+    const headlineFirst = title.children[0].children[0];
+    const headlineSecond = title.children[0].children[1];
 
-                const headlineFirst = title.children[0].children[0]
-                const headlineSecond = title.children[0].children[1]
+    const texts = document.querySelectorAll(".strip-text");
 
-                const texts = document.querySelectorAll(".strip-text")
+    gsap.to(app, { duration: 0, css: { visibility: "visible" } });
 
+    tl.from([headlineFirst.children, headlineSecond.children], {
+      duration: 1.5,
+      y: "11rem",
+      ease: "power3.easeOut",
+      stagger: {
+        each: 0.15,
+      },
+    });
 
-                gsap.to(app, {duration: 0, css: {visibility: 'visible'}})
+    var boxWidth = 500;
+    gsap.set(texts, {
+      x: function (i) {
+        return i * boxWidth;
+      },
+    });
 
-                tl.from([headlineFirst.children, headlineSecond.children], {
-                        duration: 1.5,
-                        y: "11rem",
-                        ease: "power3.easeOut",
-                        stagger: {
-                          each: .15
-                        }
-                      });
+    gsap.timeline().to(texts, {
+      //   x: -5520,
+      modifiers: {
+        x: gsap.utils.unitize((x) => parseFloat(x) % 500),
+      },
+      duration: 15,
+      ease: "none",
+      attr: { startOffset: "-2990" },
+      repeat: -1,
+    });
 
-                      var boxWidth = 500
-                  gsap.set(texts, {
-                      x:function(i) {
-                      return i * boxWidth;
-                      }
-                  });
-          
-                  gsap.timeline()
-                  .to(texts,  {
-                //   x: -5520,
-                  modifiers: {
-                      x: gsap.utils.unitize(x => parseFloat(x) % 500)
-                  },
-                      duration:15, ease:'none',
-                      attr: { startOffset: "-2990", },
-                      repeat:-1,
-                  });
-
-                //   gsap.from(title, {
-                //         duration: 1.5,
-                //         yPercent: 100,
-                //         ease: "power4",
-                //   })
-                return() => {
-                        tl.kill()
-                }
-                }, [tl])
+    //   gsap.from(title, {
+    //         duration: 1.5,
+    //         yPercent: 100,
+    //         ease: "power4",
+    //   })
+    return () => {
+      tl.kill();
+    };
+  }, [tl]);
 
   return (
-        <div 
-        className='top' ref={el => app = el}>
-                <svg width="5000" height="600" viewBox="0 0 1440 539" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-path-container">
-			<path d="M-17.4829 15.9294C297.2 -24.1211 339.25 187.141 393.211 351.93C447.171 516.719 617.113 575.553 790.539 486.617C963.965 397.68 1095.42 189.366 1488.7 319.17" stroke="#F58840" stroke-width="50" id="path-1" pathLength={5000}></path>
-			<g className='text-strip-wrapper'>
-                                <g className='texts'>
-                                <text dy="1.5%" y="15px" x='0px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Full-stack Website Developer  &nbsp; • &nbsp;
-                                        </textPath>
-                                </text>
+    <div className="top" ref={(el) => (app = el)}>
+      <svg
+        width="5000"
+        height="600"
+        viewBox="0 0 1440 539"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="text-path-container"
+      >
+        {/* <defs>
+          <radialGradient id="myGradient">
+            <stop offset="35%" stop-color="#F58840" />
+            <stop offset="80%" stop-color="#000" />
+          </radialGradient>
+        </defs> */}
+        <path
+          d="M-17.4829 15.9294C297.2 -24.1211 339.25 187.141 393.211 351.93C447.171 516.719 617.113 575.553 790.539 486.617C963.965 397.68 1095.42 189.366 1488.7 319.17"
+          stroke="#F58840"
+          stroke-width="50"
+          id="path-1"
+          pathLength={5000}
+        ></path>
+        <g className="text-strip-wrapper">
+          <g className="texts">
+            <text dy="1.5%" y="15px" x="0px">
+              <textPath href="#path-1" className="strip-text">
+                Full-stack Website Developer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x='490px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Graphic Designer &nbsp; •  &nbsp;
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="490px">
+              <textPath href="#path-1" className="strip-text">
+                Graphic Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='810px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                UI/UX Designer &nbsp; • &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="810px">
+              <textPath href="#path-1" className="strip-text">
+                UI/UX Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='1100px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Full-stack Website Developer  &nbsp; • &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="1100px">
+              <textPath href="#path-1" className="strip-text">
+                Full-stack Website Developer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='1590px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Graphic Designer &nbsp; •  &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="1590px">
+              <textPath href="#path-1" className="strip-text">
+                Graphic Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='1910px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                UI/UX Designer &nbsp; • &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="1910px">
+              <textPath href="#path-1" className="strip-text">
+                UI/UX Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x='2200px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Full-stack Website Developer  &nbsp; • &nbsp;
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="2200px">
+              <textPath href="#path-1" className="strip-text">
+                Full-stack Website Developer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x='2700px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                Graphic Designer &nbsp; •  &nbsp;
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="2700px">
+              <textPath href="#path-1" className="strip-text">
+                Graphic Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='3020px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                                UI/UX Designer &nbsp; • &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="3020px">
+              <textPath href="#path-1" className="strip-text">
+                UI/UX Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='3320px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                        Full-stack Website Developer  &nbsp; • &nbsp;
-                                        </textPath>
-                                </text>
-                                
-                                <text dy="1.5%" y="15px" x ='3810px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                        Graphic Designer &nbsp; •  &nbsp;
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="3320px">
+              <textPath href="#path-1" className="strip-text">
+                Full-stack Website Developer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='4130px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                        UI/UX Designer &nbsp; • &nbsp; 
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="3810px">
+              <textPath href="#path-1" className="strip-text">
+                Graphic Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
-                                <text dy="1.5%" y="15px" x ='4430px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                        Full-stack Website Developer  &nbsp; • &nbsp;
-                                        </textPath>
-                                </text>
-                                
-                                <text dy="1.5%" y="15px" x ='4920px'>
-                                        <textPath href="#path-1" className='strip-text'>
-                                        Graphic Designer &nbsp; •  &nbsp;
-                                        </textPath>
-                                </text>
+            <text dy="1.5%" y="15px" x="4130px">
+              <textPath href="#path-1" className="strip-text">
+                UI/UX Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
+            <text dy="1.5%" y="15px" x="4430px">
+              <textPath href="#path-1" className="strip-text">
+                Full-stack Website Developer &nbsp; • &nbsp;
+              </textPath>
+            </text>
 
+            <text dy="1.5%" y="15px" x="4920px">
+              <textPath href="#path-1" className="strip-text">
+                Graphic Designer &nbsp; • &nbsp;
+              </textPath>
+            </text>
+          </g>
+        </g>
 
-                                </g>
-                        </g>
-
-                        {/* <text dy="1.5%" y="15px" x="2150px">
+        {/* <text dy="1.5%" y="15px" x="2150px">
                                 <textPath href="#path-1" className='strip-text' ref={el => textRef2 = el}>
                                  Full-stack Website Developer  &nbsp; • &nbsp; Graphic Designer &nbsp; •  &nbsp; UI/UX Designer &nbsp; • &nbsp; Full-stack Website Developer  &nbsp; • &nbsp; Graphic Designer &nbsp; •  &nbsp; UI/UX Designer &nbsp; • &nbsp; Full-stack Website Developer  &nbsp; • &nbsp; Graphic Designer &nbsp; •  &nbsp; UI/UX Designer &nbsp; • &nbsp;
                                 </textPath>
 			</text> */}
-		</svg>
-                {/* <h1 ref={el => title = el}
+      </svg>
+      {/* <h1 ref={el => title = el}
                 data-scroll data-scroll-speed="2" className="ini-letters">T</h1> */}
-                <div className='title-container' ref={el => title= el}>
-                        <h1 data-scroll data-scroll-speed="2">
-                                <div className='line-container'>
-                                        <div className='line-container-inner'>T<span>w</span>yla</div>
-                                </div>
+      <div className="title-container" ref={(el) => (title = el)}>
+        <h1 data-scroll data-scroll-speed="2">
+          <div className="line-container">
+            <div className="line-container-inner">
+              T<span>w</span>yla
+            </div>
+          </div>
 
-                                <div className='line-container'>
-                                        <div className='line-container-inner'>Ta<span>n</span>g</div>
-                                </div>
-                        </h1>
-                </div>
+          <div className="line-container">
+            <div className="line-container-inner">
+              Ta<span>n</span>g
+            </div>
+          </div>
+        </h1>
+      </div>
 
-                {/* <div className='text'>
+      {/* <div className='text'>
                 <p
                 data-scroll data-scroll-speed="2" className='job-1'>UI/UX designer</p>
                 <p
@@ -180,8 +197,8 @@ const Hero = () => {
                 <p
                 data-scroll data-scroll-speed="2" className='job-3'>Graphic designer</p>
                 </div> */}
-        </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,43 +1,142 @@
-import React from 'react'
-import './footerStyle.scss'
+import gsap from "gsap";
+import React, { useLayoutEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/all";
+import "./footerStyle.scss";
+import useOnScreen from "../../hooks/useOnScreen";
 
 const Footer = () => {
-  return (
-    <div className='text-sec' >
-                <h3>You never know what is there waiting for you, until you take the initiative.</h3>
-                <div>
-                    <a href='/contact'>Contact form
-                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' style={{enableBackground: "new 0 0 152.9 43.4"}}>
-                        <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
-                      </svg>
-                    </a>
-                    <a href='mailto: twylaweiyutang@outlook.com'>twylaweiyutang@outlook.com
-                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' style={{enableBackground: "new 0 0 152.9 43.4", width: "380px"}}>
-                        <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
-                      </svg>
-                    </a>
-                    <a href='https://www.linkedin.com/in/weiyu-tang-297116186/' target='_blank' rel='noreferrer'>linkedin
-                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' style={{enableBackground: "new 0 0 152.9 43.4"}}>
-                        <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
-                      </svg>
-                    </a>
-                    <a href='https://www.instagram.com/twyla_weiyu_tang/' target='_blank' rel='noreferrer'>instagram
-                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' style={{enableBackground: "new 0 0 152.9 43.4"}}>
-                        <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
-                      </svg>
-                    </a>
-                </div>
-                <div className='footer-bottom'>
-                    <span
-                    data-scroll data-scroll-speed='2' data-scroll-direction="horizontal"
-                    >&copy; {new Date().getFullYear()} All Rights Reserved.</span>
-                    <span
-                    data-scroll data-scroll-speed='-2' data-scroll-direction="horizontal"
-                    >Made with &hearts; by me
-                </span>
-                </div>
-    </div>
-  )
-}
+  gsap.registerPlugin(ScrollTrigger);
+  const sec = useRef(null);
 
-export default Footer
+  const onScreen = useOnScreen(sec);
+
+  useLayoutEffect(() => {
+    let tl = gsap.timeline();
+    setTimeout(() => {
+      tl.from(sec.current, {
+        scrollTrigger: {
+          id: "slide",
+          trigger: sec.current,
+          start: "top bottom",
+          end: "bottom bottom",
+          scroller: ".App",
+          scrub: 3,
+          toggleActions: "restart pause reverse pause",
+          markers: true,
+        },
+        xPercent: -100,
+        autoAlpha: 0,
+        ease: "Power4.in",
+      });
+
+      ScrollTrigger.refresh();
+
+      return () => {
+        ScrollTrigger.getAll().forEach((instance) => {
+          instance.kill();
+        });
+      };
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="footer-wrapper">
+      <div className="overlay" />
+      <div className="text-sec" ref={sec}>
+        {/* <h3>You never know what is there waiting for you, until you take the initiative.</h3> */}
+        <div>
+          <a href="/contact" id="contact">
+            <span>Contact form</span>
+            {/* <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 152.9 43.4"
+              xmlSpace="preserve"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              style={{ enableBackground: "new 0 0 152.9 43.4" }}
+            >
+              <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4" />
+            </svg> */}
+          </a>
+          <a href="mailto: twylaweiyutang@outlook.com" id="email">
+            <span>twylaweiyutang@outlook.com</span>
+            {/* <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 152.9 43.4"
+              xmlSpace="preserve"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              style={{ enableBackground: "new 0 0 152.9 43.4", width: "380px" }}
+            >
+              <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4" />
+            </svg> */}
+          </a>
+          <a
+            href="https://www.linkedin.com/in/weiyu-tang-297116186/"
+            target="_blank"
+            rel="noreferrer"
+            id="linkedin"
+          >
+            linkedin
+            {/* <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 152.9 43.4"
+              xmlSpace="preserve"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              style={{ enableBackground: "new 0 0 152.9 43.4" }}
+            >
+              <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4" />
+            </svg> */}
+          </a>
+          <a
+            href="https://www.instagram.com/twyla_weiyu_tang/"
+            target="_blank"
+            rel="noreferrer"
+            id="instagram"
+          >
+            instagram
+            {/* <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 152.9 43.4"
+              xmlSpace="preserve"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              style={{ enableBackground: "new 0 0 152.9 43.4" }}
+            >
+              <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4" />
+            </svg> */}
+          </a>
+        </div>
+        <div className="footer-bottom">
+          <span
+            data-scroll
+            data-scroll-speed="2"
+            data-scroll-direction="horizontal"
+            id="left"
+          >
+            &copy; {new Date().getFullYear()} All Rights Reserved.
+          </span>
+          <span
+            data-scroll
+            data-scroll-speed="-2"
+            data-scroll-direction="horizontal"
+            id="right"
+          >
+            Made with &hearts; by me
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
